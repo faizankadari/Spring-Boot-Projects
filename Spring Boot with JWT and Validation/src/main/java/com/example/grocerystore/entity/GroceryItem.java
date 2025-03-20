63f6b7c8cf4037b29065aef550471146ba629e11
faizankadari <faizankadari079@gmail.com>
2025-03-20 14:42:26 +0530

    Validation Completed

--- Spring Boot with JWT and Validation/src/main/java/com/example/grocerystore/entity/GroceryItem.java
+++ Spring Boot with JWT and Validation/src/main/java/com/example/grocerystore/entity/GroceryItem.java
@@ -5,18 +5,30 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
 
-@Table(name="grocerystore_items")
+@Table(name = "grocerystore_items")
 @Data
 public class GroceryItem {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private Double price;
-    private Integer quantity;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank(message = "Item name cannot be empty")
+	private String name;
+
+	@NotBlank(message = "price cannot be empty")
+	@Min(value = 0, message = "price must be greater than 0")
+	@Max(value = 1000, message = "price should be less than 1000")
+	private Double price;
+	
+	@NotBlank(message = "quantity cannot be empty")
+	@Min(value = 0, message = "quantity must be greater than 0")
+	@Max(value = 1000, message = "quantity should be less than 1000")
+	private Integer quantity;
 }
